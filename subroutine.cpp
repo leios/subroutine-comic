2a15d66b3e07711d0985e883af1398b442b04ff9
leios <jrs.schloss@gmail.com>
2016-06-26 07:12:46 +0900

    Adding more text commands and malfunctioning consecutive harmonic function

--- subroutine.cpp
+++ subroutine.cpp
@@ -27,6 +27,17 @@ class panel{
         double xpos[num_panel], ypos[num_panel];
 };
 
+// Class for people (stick figures), arms, legs, etc, are all read from l to r.
+// Note: unfinished
+class people{
+    public: 
+        double pos_x, pos_y, head_x, head_y, arm[2], leg[2], wrist[2], shin[2],
+               width, height;
+
+        // direction person is looking -- -1 left, 0 is middle, 1 right
+        int direction;
+};
+
 // Function to draw initial layout
 void create_scene(panel &comic);
 void write_image(panel comic, std::string pngname);
@@ -42,7 +53,25 @@ void find_bpoints(std::vector<double> &p1, std::vector<double> &p2,
                   std::vector<double> &K);
 
 // Function to draw a stick figure
-void stick_figure(double height, double pos_x, double pos_y, cairo_t *ctx);
+void stick_figure(double height, double pos_x, double pos_y, cairo_t *ctx,
+                  people &person);
+
+// consec_harmonic in beta, buggy
+// Create a function for consecutive harmonic potentials
+void consec_harmonic(std::vector<double> pos_x, std::vector<double> pos_y, 
+                     double harmonic_num, int panel_num, double scale,
+                     panel &comic);
+
+// pointy_arc not implemented
+// Draw a pointy arc
+void pointy_arc(double pos_x, double pos_y, double radius, double start_angle,
+                double end_angle, double scale, int panel_num, panel &comic);
+
+// writing textbubbles
+void write_text(std::string text, people person, int panel_num, panel comic);
+
+// Create person (stickfigure for now)
+void draw_person(people &person, cairo_t *ctx);
 
 /*----------------------------------------------------------------------------//
 * MAIN
@@ -90,10 +119,14 @@ int main (){
         cairo_stroke(comic.panel_ctx[i]);
         
         // Draw stick figure
-        stick_figure(500, 1000, 1250, comic.panel_ctx[i]);
+        std::vector<people> Bob(num_panel);
+        stick_figure(500, 1000, 1250, comic.panel_ctx[i], Bob[i]);
+
+        write_text("Hello World!", Bob[i], i, comic);
 
     }
 
+/*
     // Writing "Hello World!"
     cairo_set_font_size (comic.panel_ctx[0], 100.0);
     cairo_set_source_rgb (comic.panel_ctx[0], 1.0, 1.0, 1.0);
@@ -108,94 +141,21 @@ int main (){
     x_line[2] = 750; y_line[2] = 800;
 
     draw_array(x_line, y_line, 0, comic);
-    cairo_stroke(comic.panel_ctx[0]);
 
     cairo_stroke(comic.panel_ctx[0]);
+*/
+
+    // Create array for consecutive harmonic segment
+    //std::vector<double> pos_x(3), pos_y(3);
+    //pos_x[0] = 0; pos_y[0] = 500;
+    //pos_x[1] = 1000; pos_y[1] = 1000;
+    //pos_x[2] = 2000; pos_y[2] = 1500;
+
+    //consec_harmonic(pos_x, pos_y, 10, 0, .01, comic);
 
     write_image(comic, "subroutine.png");
 
     destroy_all(comic);
-
-
-    /*------------------------------------------------------------------------//
-    * TESTING
-    *-------------------------------------------------------------------------*/
-
-    /*
-
-    // Create a x^2 function and write to image
-    // Note that y is inverted
-    std::vector <double> x(10), y(10);
-    std::cout << "array points are:" << '\n';
-    for (size_t i = 0; i < x.size(); i++){
-        x[i] = i * (1000 / x.size());
-        //y[i] = 0.001 * (x[i]) * (x[i]);
-        y[i] = abs((0.001 * (x[i]) * (x[i])) - comic.box_width);
-        std::cout << x[i] << '\t' << y[i] << '\n';
-    }
-
-    draw_array(x, y, 0, comic);
-
-    cairo_move_to(comic.panel_ctx[0],x[0],y[0]);
-    for (size_t i = 0; i < x.size(); ++i){
-        cairo_line_to(comic.panel_ctx[0], x[i], y[i]);
-    }
-
-    // Creating initial surface to draw on.
-    cairo_surface_t *surface =
-        cairo_image_surface_create (CAIRO_FORMAT_ARGB32, 1000, 400);
-    cairo_t *cr =
-        cairo_create (surface);
-
-    cairo_surface_t *surface_2 = 
-        cairo_image_surface_create (CAIRO_FORMAT_ARGB32, 550, 150);
-
-    cairo_t *cr_2 = cairo_create(surface_2);
-
-    cairo_select_font_face (cr, "serif", CAIRO_FONT_SLANT_NORMAL, 
-                            CAIRO_FONT_WEIGHT_BOLD);
-    cairo_set_source_rgb (cr, 0, 0.0, 1.0);
-
-    // Draws a line of width pixels thick
-    int width = 50;
-    cairo_move_to (cr, 0, 0);
-    cairo_line_to (cr, 1000, 0);
-    cairo_set_line_width (cr, width);
-    cairo_stroke(cr);
-
-    cairo_set_font_size (cr, 12.0);
-    cairo_set_source_rgb (cr, 1.0, 1.0, 1.0);
-    cairo_move_to (cr, 0.0, 25.0);
-    cairo_show_text (cr, "Hello, world");
-
-    // Create a rectangle
-    cairo_set_source_rgb(cr, 0, 0, 1);
-    cairo_rectangle(cr, 0, 50, 500, 100);
-    cairo_fill(cr);
-
-    cairo_rectangle(cr, 0, 200, 500, 100);
-
-    cairo_rectangle(cr_2, 5, 5, 50, 10);
-    cairo_stroke (cr_2);
-    cairo_set_font_size (cr_2, 12.0);
-    cairo_set_source_rgb (cr_2, 1.0, 0, 1.0);
-    cairo_move_to (cr_2, 0.0, 25.0);
-    cairo_show_text (cr_2, "Hello, world");
-
-    cairo_stroke(cr);
-
-    cairo_set_source_surface (cr, surface_2, 500, 200);
-
-    cairo_paint(cr);
-
-    // writing and destroying rendering device
-    cairo_destroy (cr);
-    cairo_destroy (cr_2);
-    cairo_surface_write_to_png (surface, "subroutine.png");
-    cairo_surface_write_to_png (surface_2, "sub_2.png");
-    cairo_surface_destroy (surface);
-    cairo_surface_destroy (surface_2);
-    */
 
 }
 
@@ -470,7 +430,8 @@ void find_bpoints(std::vector<double> &p1, std::vector<double> &p2,
 
 // Function to draw a stick figure
 // Note: position is for the center of stick figure
-void stick_figure(double height, double pos_x, double pos_y, cairo_t *ctx){
+void stick_figure(double height, double pos_x, double pos_y, cairo_t *ctx,
+                  people &person){
 
     // Draw head
     double head_y = pos_y - height * 0.333;
@@ -501,6 +462,137 @@ void stick_figure(double height, double pos_x, double pos_y, cairo_t *ctx){
     cairo_move_to(ctx, pos_x, pos_y - 0.16666 * height);
     cairo_line_to(ctx, pos_x - 0.05 * height, crotch_y);
 
+    // Giving person identity
+    person.head_x = head_x;
+    person.head_y = head_y;
+    person.height = height;
+    person.width = 2 * 0.16666 *  height;
+    person.direction = -1;
+    person.pos_x = pos_x;
+    person.pos_y = pos_y;
+
+    // Should draw arm and leg in future.
+
     cairo_stroke(ctx);
+}
+
+// Note: not working in current state. Requires straight line along horizontal
+void consec_harmonic(std::vector<double> pos_x, std::vector<double> pos_y, 
+                     double harmonic_num, int panel_num, double scale,
+                     panel &comic){
+
+    // determining length of consecutive harmonic segment
+    double length = 0;
+    std::vector<double> tot_length(pos_x.size());
+    tot_length[0] = 0;
+    for (size_t i = 1; i < pos_x.size(); ++i){
+        length += sqrt((pos_x[i] - pos_x[i-1]) * (pos_x[i] - pos_x[i-1])
+                       - (pos_y[i] - pos_y[i-1]) * (pos_y[i] - pos_y[i-1])); 
+        tot_length[i] = length;
+    }
+
+    // Position of well
+    double well_pos = (length / (2*(double)harmonic_num));
+    double xtick, slope;
+    int res = 10, length_index = 0;
+    std::vector<double> x(res+1), y(res+1);
+
+    // Drawing consecutive potentials
+    for (int i = 0; i < harmonic_num; ++i){
+        for (int j = 0; j <= res; ++j){
+
+            xtick = (double)j * length / (double)harmonic_num / (double)res;
+            y[j] = -scale * (xtick - well_pos) * (xtick - well_pos);
+            x[j] = xtick + (double)i * length / (double)harmonic_num;
+            //std::cout << xtick << '\t' << i << '\t' << length << '\t' 
+            //          << harmonic_num << '\t' << x[j] << '\n';
+
+            // Adding in x and y positions from the read in array
+            // Find the segment of the provided array that we are closest to
+            for (size_t k = 1; k < tot_length.size(); ++k){
+                if (x[j] >= tot_length[k-1] && x[j] < tot_length[k]){
+                    length_index = k-1;
+                    std::cout << k << '\n';
+                    //continue;
+                }
+            }
+
+            // Probably breaking here.
+            // finding slope of length_index / length_index - 1
+            slope = (pos_y[length_index+1] - pos_y[length_index])
+                    / (pos_x[length_index+1] - pos_x[length_index]);
+
+            y[j] += pos_y[length_index] + slope * x[j];
+
+            std::cout << pos_y[length_index] << '\t' << pos_x[length_index]
+                      << '\t' << slope << '\t' << length_index << '\n';
+            
+            
+            std::cout << xtick << '\t' << x[j] << '\t' << y[j] << '\n';
+
+        }
+
+        draw_array(x, y, panel_num, comic);
+        cairo_stroke(comic.panel_ctx[panel_num]);
+
+    }
+
+}
+
+// writing textbubbles
+void write_text(std::string text, people person, int panel_num, panel comic){
+
+    // Determing position relative to top of panel
+    double overhead = comic.box_height - person.head_y;
+
+    // Determine size in x of box
+    double text_width = comic.box_width / 3.0, text_x;
+
+    // Determing x position of text
+    if(person.direction == -1){
+        text_x = person.pos_x - 1.5 * person.width;
+    }
+    else if(person.direction == 0){
+        text_x = person.pos_x;
+    }
+    else{
+        text_x = person.pos_x + 1.5 * person.width;
+    }
+
+    // Determing how to parse the text before detemining y pos
+    // COMING SOON!
+
+    double text_y = person.head_y - overhead / 3.0;
+
+    // Draw line from person to text
+    std::vector<double> x_line(3), y_line(3);
+
+    x_line[0] = person.head_x + 0.75 * (double)person.direction * person.width;
+    x_line[2] = text_x;
+    x_line[1] = ((x_line[2] + x_line[0]) / 2)
+                + (double)person.direction * comic.box_width / 100.0;
+
+    y_line[0] = person.head_y - comic.box_height / 50.0;
+    y_line[2] = text_y + comic.box_height / 50.0;
+    y_line[1] = ((y_line[2] + y_line[0]) / 2.0) + comic.box_height / 100.0;
+
+    for (int i = 0; i < 3; ++i){
+        std::cout << x_line[i] << '\t' << y_line[i] << '\n';
+    }
+    
+    draw_array(x_line, y_line, panel_num, comic);
+
+    // Draw text
+    cairo_set_font_size (comic.panel_ctx[0], 100.0);
+    cairo_set_source_rgb (comic.panel_ctx[0], 1.0, 1.0, 1.0);
+
+    // Determining where to move to
+    cairo_text_extents_t textbox;
+    cairo_text_extents(comic.panel_ctx[panel_num], text.c_str(), &textbox);
+    cairo_move_to (comic.panel_ctx[0], text_x - textbox.width / 2.0, text_y);
+    cairo_show_text (comic.panel_ctx[0], text.c_str());
+
+    cairo_stroke(comic.panel_ctx[panel_num]);
+
 }
 
